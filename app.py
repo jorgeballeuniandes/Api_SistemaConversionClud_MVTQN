42c9b4bb75bc76f33f9fa30d8651e3d817ea8513
Jose Manuel Vega <jm.vegaa1@uniandes.edu.co>
2022-10-23 18:09:29 -0500

    solucion de conflictos 2

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ db.create_all()
 cors = CORS(app)
 
 api = Api(app)
-api.add_resource(VistaSignIn, '/signin')
-api.add_resource(VistaLogIn, '/login')
+
 api.add_resource(Task_create,'/newTask')
 api.add_resource(Subir_archivos,'/uploadFile')
 api.add_resource(VistaSignIn, '/auth/signup')
