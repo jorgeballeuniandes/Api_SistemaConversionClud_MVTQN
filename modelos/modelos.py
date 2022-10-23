dac6f2c5ccfa2a5cdf649c488cfc2bfa6883ac3e
jorgeballeuniandes <j.ballesterosv@uniandes.edu.co>
2022-10-23 13:59:46 -0500

    base app

---
+++ modelos/modelos.py
@@ -0,0 +1,46 @@
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields, Schema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+db = SQLAlchemy()
+
+
+
+
+
+class Tarea(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre_archivo = db.Column(db.String(128))
+    nuveo_formato = db.Column(db.String(30))
+    estado = db.Column(db.String(30))
+    time_stamp = db.Column(db.DateTime)
+    
+    usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+
+
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(50))
+    contrasena = db.Column(db.String(50))
+    email = db.Column(db.String(200))
+    tareas = db.relationship('Tarea', cascade='all, delete, delete-orphan')
+
+
+
+
+
+class TareaSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Tarea
+        include_relationships = True
+        load_instance = True
+
+
+
+class UsuarioSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuario
+        include_relationships = True
+        load_instance = True
---
+++ vistas/vistas.py
@@ -0,0 +1,46 @@
+from flask import request
+from flask_jwt_extended import jwt_required, create_access_token
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
+from modelos import db, Tarea, Usuario
+from modelos.modelos import TareaSchema, UsuarioSchema
+
+tarea_schema = TareaSchema()
+usuario_schema = UsuarioSchema()
+
+
+class VistaSignIn(Resource):
+
+    def post(self):
+        nuevo_usuario = Usuario(usuario=request.json["usuario"], contrasena=request.json["contrasena"])
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        token_de_acceso = create_access_token(identity=nuevo_usuario.id)
+        return {"mensaje": "usuario creado exitosamente", "token": token_de_acceso, "id": nuevo_usuario.id}
+
+    def put(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        usuario.contrasena = request.json.get("contrasena", usuario.contrasena)
+        db.session.commit()
+        return usuario_schema.dump(usuario)
+
+    def delete(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        db.session.delete(usuario)
+        db.session.commit()
+        return '', 204
+
+
+class VistaLogIn(Resource):
+
+    def post(self):
+        usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"],
+                                       Usuario.contrasena == request.json["contrasena"]).first()
+        db.session.commit()
+        if usuario is None:
+            return "El usuario no existe", 404
+        else:
+            token_de_acceso = create_access_token(identity=usuario.id)
+            return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso}
+
