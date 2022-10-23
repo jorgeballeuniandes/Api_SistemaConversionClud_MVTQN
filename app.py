b9a6ad1302b79ff307651d0968d5e5bad4477fec
Jose Manuel Vega <jm.vegaa1@uniandes.edu.co>
2022-10-23 17:51:04 -0500

    se crea endpoitn para crear tarea y otro pra cargar el alrchivo de audio

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 from modelos import db
-from vistas import VistaSignIn, VistaLogIn
+from vistas import VistaSignIn, VistaLogIn,Task_create,Subir_archivos
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///conversiones.db'
@@ -23,5 +24,7 @@ cors = CORS(app)
 api = Api(app)
 api.add_resource(VistaSignIn, '/signin')
 api.add_resource(VistaLogIn, '/login')
+api.add_resource(Task_create,'/newTask')
+api.add_resource(Subir_archivos,'/uploadFile')
 
 jwt = JWTManager(app)
